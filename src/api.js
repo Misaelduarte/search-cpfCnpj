ef02516762485527b9f789b6d74136d066fbcf21
Misael Duarte <developer.md775@gmail.com>
2022-10-31 19:24:44 -0300

    chore: Add variable port

--- src/api.js
+++ src/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://search-tj.herokuapp.com/"
+      ? "https://node-search-tj.herokuapp.com/"
       : "http://localhost:7777/",
 });
